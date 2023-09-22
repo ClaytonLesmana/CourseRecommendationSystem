8fa114c85820c0826f37bcd0b62dc92624579c79
claytonlesmana <claytonlesmana0808@gmail.com>
2023-09-22 10:33:23 +1000

    Modified CourseMatcher Pages

--- src/Pages/CourseMatcher.jsx
+++ src/Pages/CourseMatcher.jsx
@@ -1,11 +1,21 @@
+// import Button from "./Components/Navbar";
+import "../Styles/main.css";
 function CourseMatcher() {
   return (
-    <>
-      <h1>
-        Find out which careers, languages, and courses suit your personal
-        interests and strengths best.
-      </h1>
-    </>
+    <div className="course-matcher-container">
+      <div className="questions-section">
+        <h3 className="question-text">What's your programming personality?</h3>
+      </div>
+      <div className="answers-section">
+        <p className="answer-text">
+          Find out which careers, languages, and courses suit your personal
+          interests and strengths best.
+        </p>
+        <button type="button" class="btn btn-light">
+          Light
+        </button>
+      </div>
+    </div>
   );
 }
 
