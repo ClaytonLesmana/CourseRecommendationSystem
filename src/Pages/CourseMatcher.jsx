c8910e4769ce56beaa6c838e3cfd4ea1b1b4385e
claytonlesmana <claytonlesmana0808@gmail.com>
2023-09-24 16:00:00 +1000

    CourseMatcher Questionaire BETA Update

--- src/Pages/CourseMatcher.jsx
+++ src/Pages/CourseMatcher.jsx
@@ -1,20 +1,92 @@
-// import Button from "./Components/Navbar";
+import React, { useState } from "react";
+import { ScrollRestoration } from "react-router-dom";
 import "../Styles/main.css";
 function CourseMatcher() {
+  const questions = [
+    {
+      questionText: "What's your programming personality?",
+      answerOptions: [
+        { answerText: "New York", isCorrect: false },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "Dublin", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is CEO of Tesla?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "The iPhone was created by which company?",
+      answerOptions: [
+        { answerText: "Apple", isCorrect: true },
+        { answerText: "Intel", isCorrect: false },
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Microsoft", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "How many Harry Potter books are there?",
+      answerOptions: [
+        { answerText: "1", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [showScore, setShowScore] = useState(false);
+
+  const [score, setScore] = useState(0);
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
   return (
     <div className="course-matcher-container">
-      <div className="questions-section">
-        <h3 className="question-text">What's your programming personality?</h3>
-      </div>
-      <div className="answers-section">
-        <p className="answer-text">
-          Find out which careers, languages, and courses suit your personal
-          interests and strengths best.
-        </p>
-        <button type="button" class="btn btn-light">
-          Light
-        </button>
-      </div>
+      {showScore ? (
+        <div className="score-sction">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="questions-section">
+            <div className="question-count">
+              <span>{currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+
+          <div className="answers-section">
+            {questions[currentQuestion].answerOptions.map((answerOptions) => (
+              <button
+                onClick={() => handleAnswerButtonClick(answerOptions.isCorrect)}
+                className="button-questionaire"
+              >
+                {answerOptions.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
