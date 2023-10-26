828e13c589e703a543181f89e488b0214171f701
Alper Demirbilek <alperdemirbilek12@gmail.com>
2023-10-26 23:50:17 +1100

    Some layout changes

--- src/Pages/Course.jsx
+++ src/Pages/Course.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import "../Styles/courseOutput.css";
+
+
 function Course() {
     const { courseName } = useParams();
     const [courseDetails, setCourseDetails] = useState({});
@@ -15,6 +17,18 @@ function Course() {
 
     return (
         <div>
+            <div className="output-container">
+                <div className="progress-bar-placeholder">
+                    <h2 className="major-header"> {courseName || "Default Major"}</h2>
+
+                    <div className="progress-labels-container">
+                        <h5 className="progress-label-1">Basics</h5>
+                        <h5 className="progress-label-2">Advance Topics</h5>
+                        <h5 className="progress-label-3">Mastery</h5>
+                    </div>
+                </div>
+            </div>
+
             <h1 className="major-header">{courseName}</h1>
             <h2>Subjects:</h2>
             <ul>
@@ -25,6 +39,10 @@ function Course() {
                         </li>
                     ))}
             </ul>
+
+
+
+
         </div>
     );
 }
--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -1,23 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'; // Import Link
+import { Link } from 'react-router-dom';
+import '../Styles/Courses.css'; // Import your CSS file
 
 function Courses() {
-  const [courses, setCourses] = useState([]);
+    const [courses, setCourses] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/courses') // This will send a request to your Node.js server
-      .then((response) => response.json())
-      .then((data) => setCourses(data.courses))
-      .catch((error) => console.error(error));
-  }, []);
+    useEffect(() => {
+        fetch('http://localhost:3000/courses')
+            .then((response) => response.json())
+            .then((data) => setCourses(data.courses))
+            .catch((error) => console.error(error));
+    }, []);
 
     return (
-        <div>
-            <h1>Courses</h1>
-            <ul>
+        <div className="courses-container">
+            <h1 className="courses-header">Course List</h1>
+            <ul className="courses-list">
                 {courses.map((course) => (
-                    <li key={course.course_num}>
-                        <Link to={`/course/${course.course_name}`}>{course.course_name}</Link>
+                    <li key={course.course_num} className="course-item">
+                        <Link to={`/course/${course.course_name}`} className="course-link">
+                            {course.course_name}
+                        </Link>
                     </li>
                 ))}
             </ul>
